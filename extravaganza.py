f335ec3a8f9e97399cf256bd9fa11f08e6d58c15
someone <some@n.e>
2016-10-27 20:19:26 +0100

    small script to search when we found the obstacle

--- extravaganza.py
+++ extravaganza.py
@@ -6,7 +6,20 @@ Small example how to analyse a rosbag
 We are trying to find out where we first detected the obstacle!
 """
 
-
 extravaganzaBag = rosbag.Bag("extravaganza.bag")
-message_stream = iter(extravaganzaBag.read_messages():
+for topic, msg, t in extravaganzaBag.read_messages():
+    if topic == "/camera_detection":
+        if msg.data == "detected":
+            detectionTime = t
+            break
+
+for topic, msg, t in extravaganzaBag.read_messages():
+    if topic == "/position":
+        if t > detectionTime:
+            timeReached = True
+            print("The position of the boat when an obstacle was detected was: \n"
+                +"Latitude: " + str(msg.latitude) + "\nLongitude: " + str(msg.longitude))
+            break
+
+
     
