5bc7b8250315ba78432a69e231f73d9dc9483252
Martin Biggs <martin@jamak9.co.uk>
2016-10-28 08:17:44 +0000

    Added my version of the script

--- extravaganza.py
+++ extravaganza.py
@@ -21,5 +21,21 @@ for topic, msg, t in extravaganzaBag.read_messages():
                 +"Latitude: " + str(msg.latitude) + "\nLongitude: " + str(msg.longitude))
             break
 
+# Here is my my version of this script.
+# It took me a little while to work out how to access and compare the messages.
+# I get a slightly different result to the above script as I am taking the position immediatly before
+# the detection and the above script is taking the position immediatly after the detection.
+
+extravaganza_bag = rosbag.Bag('extravaganza.bag')
+position = None
+
+for topic, msg, t in extravaganza_bag.read_messages(topics=['/camera_detection', '/position']):
+    if topic == '/position':
+        position = msg
+    elif msg.data == 'detected':
+        print ('First detection at latitude:{} longitude:{}'.format(position.latitude, position.longitude))
+        break
+else:
+    print('Detection event not found')
 
     
